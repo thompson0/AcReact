2697976ade4ecc90b4fe6cbe51d955dbc2bdf9e5
thompson0 <thompsonjooj@gmail.com>
2024-04-07 18:49:57 -0300

    Banner

---
+++ Ac2react/src/Componentes/banner/banner.jsx
@@ -0,0 +1,12 @@
+function Banner(){
+    return(
+        <div className="banner">
+        <h1>
+           Bem-vindo ao Portifolio Joao Vitor Lombardi!
+        </h1>
+
+        </div>
+    )
+}
+
+export default Banner
